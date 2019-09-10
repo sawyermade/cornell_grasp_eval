37fd913ddf6284611886f96968ada59457966007
Daniel Sawyer <sawyermade0@gmail.com>
2019-09-09 22:24:40 -0400

    tersed_code

--- parse_cornell.py
+++ parse_cornell.py
@@ -1,5 +1,6 @@
 import os, sys, re, json, cv2, math
 
+# Gets the rgb paths for the 885 pngs
 def get_rgb_paths(dataset_dir_path):
 	# rgb filename regex
 	reg_str = r'^pcd(\d{4})r\.png$'
@@ -10,14 +11,15 @@ def get_rgb_paths(dataset_dir_path):
 	for root, dirs, files in os.walk(dataset_dir_path):
 		# Gets all rgb images and sorts by image number
 		if files:
-			files = [f for f in files if reg.match(f)]
-			files.sort(key=lambda x: int(reg.match(x).group(1)))
-			path_list += [os.path.join(root, f) for f in files]
+			files_rgb = [f for f in files if reg.match(f)]
+			files_rgb.sort(key=lambda x: int(reg.match(x).group(1)))
+			path_list += [os.path.join(root, f) for f in files_rgb]
 
 	# Sorts by dir number 01-10 and returns
 	path_list.sort(key=lambda x: int(x.split(os.sep)[-2]))
 	return path_list
 
+# Gets rectangle file paths for pos/neg
 def get_rec_paths(dataset_dir_path):
 	# rec filename regex
 	reg_str_pos = r'^pcd(\d{4})cpos\.txt$'
@@ -43,62 +45,48 @@ def get_rec_paths(dataset_dir_path):
 	path_list_neg.sort(key=lambda x: int(x.split(os.sep)[-2]))
 	return path_list_pos, path_list_neg
 
+# Find valid rectangle points from gt files and store to list
+def find_rec_points(path):
+	with open(path) as gtf:
+		# Goes through positive points
+		rec_pts = []
+		rec_list = []
+		for num, line in enumerate(gtf):
+			# Gets point, converts to float tuple, and stores it
+			line_split = line.strip('\n').split(' ')
+			point = tuple([float(p) for p in line_split[:2]])
+			rec_pts.append(point)
+
+			# Checks if last rectangle point or not
+			if num != 0 and (num+1) % 4 == 0:
+				# Adds rectangle to list if no NaNs and sets nan flag
+				nan_flag = False
+				for ps in rec_pts:
+					for p in ps:
+						if math.isnan(p):
+							nan_flag = True
+							break
+					if nan_flag:
+						break
+
+				# Checks for NaN's to discard and adds if not
+				if not nan_flag:
+					rec_list.append(rec_pts)
+				rec_pts = []
+
+		# Returns rectangles
+		return rec_list
+
+''' Makes ground truth dictionary, 
+key is rgb file path and contains dicts for pos/neg keys with 3d list of rectangle points'''
 def make_gt_dict(path_list_rgb, path_list_pos, path_list_neg):
 	# Goes through all rgb/recs
 	gt_dict = {}
 	for path_rgb, path_pos, path_neg in zip(path_list_rgb, path_list_pos, path_list_neg):
-		# Opens rec files
-		pos_rec_list, neg_rec_list = [], []
-		with open(path_pos) as posf, open(path_neg) as negf:
-			# Goes through positive points
-			rec_pts = []
-			for num, line in enumerate(posf):
-				# Gets point
-				line_split = line.strip('\n').strip(' ').split(' ')
-				point = tuple([float(p) for p in line_split])
-				rec_pts.append(point)
+		# Gets all valid positive and negative rectangles
+		pos_rec_list, neg_rec_list = find_rec_points(path_pos), find_rec_points(path_neg)
 
-				# Checks if new rectangle or not
-				if num != 0 and (num+1) % 4 == 0:
-					# Adds rectangle to list if no NaNs, clears for new rec
-					nan_flag = False
-					for ps in rec_pts:
-						for p in ps:
-							if math.isnan(p):
-								nan_flag = True
-								break
-						if nan_flag:
-							break
-
-					if not nan_flag:
-						pos_rec_list.append(rec_pts)
-					rec_pts = []
-
-			# Goes through negative points
-			rec_pts = []
-			for num, line in enumerate(negf):
-				# Gets point
-				line_split = line.strip('\n').strip(' ').split(' ')
-				point = tuple([float(p) for p in line_split])
-				rec_pts.append(point)
-
-				# Checks if new rectangle or not
-				if num != 0 and (num+1) % 4 == 0:
-					# Adds rectangle to list if no NaNs, cleas for new rec
-					nan_flag = False
-					for ps in rec_pts:
-						for p in ps:
-							if math.isnan(p):
-								nan_flag = True
-								break
-						if nan_flag:
-							break
-
-					if not nan_flag:
-						neg_rec_list.append(rec_pts)
-					rec_pts = []
-
-		# Adds to gt_dict
+		# Adds recs to gt_dict
 		gt_dict.update({
 			path_rgb : {
 				'pos' : pos_rec_list,
@@ -109,7 +97,8 @@ def make_gt_dict(path_list_rgb, path_list_pos, path_list_neg):
 	# Returns ground truth dictionary
 	return gt_dict
 
-def create_vis(vis_dir_path, gt_dict):
+# Creates visualization of ground truth pos/neg rectangles
+def create_vis(vis_dir_path, gt_dict, incl_negs=False):
 	# rgb filename regex
 	reg_str = r'^pcd(\d{4})r\.png$'
 	reg = re.compile(reg_str)
@@ -119,7 +108,7 @@ def create_vis(vis_dir_path, gt_dict):
 		# Opens image
 		img = cv2.imread(rgb_path, -1)
 
-		# Draws pos recs
+		# Draws pos recs green bgr
 		color = (0, 255, 0)
 		for rec in rec_dict['pos']:
 			if len(rec) < 4: print(rgb_path, rec)
@@ -129,14 +118,15 @@ def create_vis(vis_dir_path, gt_dict):
 			cv2.line(img, p3, p4, color)
 			cv2.line(img, p4, p1, color)
 
-		# Draws neg recs
-		color = (0, 0, 255)
-		for rec in rec_dict['neg']:
-			p1, p2, p3, p4 = [(round(r[0]), round(r[1])) for r in rec]
-			cv2.line(img, p1, p2, color)
-			cv2.line(img, p2, p3, color)
-			cv2.line(img, p3, p4, color)
-			cv2.line(img, p4, p1, color)
+		# Draws neg recs red bgr
+		if incl_negs:
+			color = (0, 0, 255)
+			for rec in rec_dict['neg']:
+				p1, p2, p3, p4 = [(round(r[0]), round(r[1])) for r in rec]
+				cv2.line(img, p1, p2, color)
+				cv2.line(img, p2, p3, color)
+				cv2.line(img, p3, p4, color)
+				cv2.line(img, p4, p1, color)
 
 		# Checks vis path
 		if not os.path.exists(vis_dir_path):
@@ -149,7 +139,10 @@ def create_vis(vis_dir_path, gt_dict):
 		out_path = os.path.join(vis_dir_path, fname)
 		cv2.imwrite(out_path, img)
 
-def main(dataset_dir_path, vis_dir_path):
+	# Completed successfully
+	return True
+
+def main(dataset_dir_path, vis_dir_path=None, incl_negs=False):
 	# Gets all rgb png paths
 	path_list_rgb = get_rgb_paths(dataset_dir_path)
 
@@ -161,16 +154,19 @@ def main(dataset_dir_path, vis_dir_path):
 	
 	# If vis
 	if vis_dir_path:
-		create_vis(vis_dir_path, gt_dict)
+		create_vis(vis_dir_path, gt_dict, incl_negs)
 
 	return gt_dict
 
 if __name__ == '__main__':
 	# Args, main dataset path and vis output path
+	incl_negs = False
 	dataset_dir_path = sys.argv[1]
 	if len(sys.argv) > 2:
 		vis_dir_path = sys.argv[2]
+		if len(sys.argv) > 3:
+			incl_negs = True
 	else: vis_dir_path = None
 
 	# Runs main
-	main(dataset_dir_path, vis_dir_path)
+	main(dataset_dir_path, vis_dir_path, incl_negs)
